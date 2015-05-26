cedbc6d094dc2259d4d325110b858e8fc0b241b7
skwak <skwak@4e4e4688-dcad-144b-b5e7-53e9baafb3f0>
2015-05-26 09:22:05 +0000

    ACE로 _popen을 대체할 만한 방법을 찾아봤으나, 직접적인 함수는 없고 대체적인 방법을 검토하려다 그냥 윈도우 함수인 _popen을 사용하여 샘플을 구현

--- fEvtRcv/main.cpp
+++ fEvtRcv/main.cpp
@@ -1,8 +1,22 @@
 #include <ace/Log_Msg.h>
+#include <stdio.h>
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
 	ACE_TRACE(ACE_TEXT("main"));
+
+	char   psBuffer[128];
+	FILE   *pPipe;
+
+	if ((pPipe = _popen("dir c:\\*.*", "rt")) == NULL)
+		ACE_ERROR((LM_ERROR, "(%t)\n", "_popen() error", -1));
+
+	while (fgets(psBuffer, 128, pPipe))
+	{
+		printf(psBuffer);
+	}
+	feof(pPipe);
+
 	ACE_DEBUG((LM_INFO, ACE_TEXT("(%t) end of main\n")));
 	ACE_RETURN(0);
 }
